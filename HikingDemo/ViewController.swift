ea06f03945b263bff8fb28fc55b9195ceebea7ba
yanhongkit <yanhongkit@yahoo.com.hk>
2017-11-13 07:54:36 +0800

    commit main page button

--- HikingDemo/ViewController.swift
+++ HikingDemo/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func mapPageBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func weatherPageBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func safariPageBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func sosPageBtn(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
